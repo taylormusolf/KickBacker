4ed8c1321515a86440632e87ad79d153f74cab21
Taylor Musolf <tcmusolf@gmail.com>
2023-02-05 11:49:58 -0800

    Update to ruby 3.1.1 and rails 7.0.3' '

--- db/schema.rb
+++ db/schema.rb
@@ -2,16 +2,15 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_18_230241) do
-
+ActiveRecord::Schema[7.0].define(version: 2023_02_05_194623) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,7 +19,7 @@ ActiveRecord::Schema.define(version: 2021_05_18_230241) do
     t.string "record_type", null: false
     t.bigint "record_id", null: false
     t.bigint "blob_id", null: false
-    t.datetime "created_at", null: false
+    t.datetime "created_at", precision: nil, null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
@@ -31,17 +30,24 @@ ActiveRecord::Schema.define(version: 2021_05_18_230241) do
     t.string "content_type"
     t.text "metadata"
     t.bigint "byte_size", null: false
-    t.string "checksum", null: false
-    t.datetime "created_at", null: false
+    t.string "checksum"
+    t.datetime "created_at", precision: nil, null: false
+    t.string "service_name", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "active_storage_variant_records", force: :cascade do |t|
+    t.bigint "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
   create_table "backings", force: :cascade do |t|
     t.float "amount_pledged", null: false
     t.integer "backer_id", null: false
     t.integer "project_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.integer "reward_id"
     t.index ["backer_id", "project_id"], name: "index_backings_on_backer_id_and_project_id", unique: true
     t.index ["backer_id"], name: "index_backings_on_backer_id"
@@ -50,8 +56,8 @@ ActiveRecord::Schema.define(version: 2021_05_18_230241) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.string "description"
     t.index ["name"], name: "index_categories_on_name", unique: true
   end
@@ -63,12 +69,12 @@ ActiveRecord::Schema.define(version: 2021_05_18_230241) do
     t.string "updates"
     t.string "faq"
     t.string "location", null: false
-    t.datetime "start_date", null: false
-    t.datetime "end_date", null: false
+    t.datetime "start_date", precision: nil, null: false
+    t.datetime "end_date", precision: nil, null: false
     t.float "funding_goal", null: false
     t.integer "creator_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.integer "category_id"
     t.index ["category_id"], name: "index_projects_on_category_id"
     t.index ["creator_id"], name: "index_projects_on_creator_id"
@@ -80,8 +86,8 @@ ActiveRecord::Schema.define(version: 2021_05_18_230241) do
     t.string "description", null: false
     t.integer "project_id", null: false
     t.float "cost", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.index ["project_id"], name: "index_rewards_on_project_id"
   end
 
@@ -89,8 +95,8 @@ ActiveRecord::Schema.define(version: 2021_05_18_230241) do
     t.string "username", null: false
     t.string "password_digest", null: false
     t.string "session_token", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.string "email", null: false
     t.string "bio"
     t.index ["email"], name: "index_users_on_email", unique: true
@@ -99,4 +105,5 @@ ActiveRecord::Schema.define(version: 2021_05_18_230241) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
 end
