763f05b27a250e2d1a2f77621d45a3693d8aa3d8
Taylor Musolf <tcmusolf@gmail.com>
2023-09-01 15:45:59 -0700

    Style project form

--- frontend/components/project_form/project_form.jsx
+++ frontend/components/project_form/project_form.jsx
@@ -10,7 +10,7 @@ export default function ProjectForm(){
   const history = useHistory();
   const location = useLocation();
   const [errors, setErrors] = useState([]);
-  const [page, setPage] = useState(1);
+  const [page, setPage] = useState(2);
   const [project, setProject] = useState({
     title: '',
     category_id: '',
@@ -27,9 +27,10 @@ export default function ProjectForm(){
   const {projectId} = useParams();
   const oldProject = useSelector((state) => state?.entities?.projects ? state.entities.projects[projectId] : null);
   const currentUser = useSelector((state) => state.session);
+  const formType = projectId ? 'update' : 'create';
 
   useEffect(()=>{
-    resetProjectErrors();
+    // dispatch(resetProjectErrors());
     handScroll();
     if(projectId){
       dispatch(fetchProject(projectId));
@@ -120,16 +121,20 @@ export default function ProjectForm(){
   }
 
   // if (!project) return null;
-  const preview = project.imageUrl? <img src={project.imageUrl} alt='new'/> : oldProject?.photo_url ? <img src={oldProject.photo_url} alt='old'/> : null
+  const preview = project.imageUrl? <img className='new-project-img-preview' src={project.imageUrl} alt='new'/> : oldProject?.photo_url ? <img className='new-project-img-preview' src={oldProject.photo_url} alt='old'/> : null
   
   const formContents = () => {
     if(page === 1){
       return(
         <>
-          <label>Project Category
+          {page === 1 && formType === 'create' && <><h1 className='new-project-first'>First, let’s get you set up.</h1></>}
+          <h1 className="new-project-title">Select a primary category for your project.</h1>
+          <h2 className="new-project-subtitle">These will help backers find your project, and you can change them later if you need to.</h2>
+          {/* <label>Project Category */}
             <br/>
-            {(!projectId || project?.category_id) && <select defaultValue={project.category_id ? `${project.category_id}` : ''} className='new-project-categories' onChange={update('category_id')}>
-              <option value='' disabled={true}>Select your category</option>
+            {(!projectId || project?.category_id) && 
+            <select defaultValue={project.category_id ? `${project.category_id}` : ''} className='new-project-categories' onChange={update('category_id')}>
+              <option value='' disabled={true}>Select</option>
               <option value="1">Art</option>
               <option value="2">Comics & Illustration</option>
               <option value="3">Design & Tech</option>
@@ -140,60 +145,71 @@ export default function ProjectForm(){
               <option value="8">Publishing</option>
             </select>}
             <div className='project-errors'>{renderErrors()}</div>
-          </label>
-          <h1>Getting started with your project!</h1>
-          <button onClick={()=> !project?.category_id ? setErrors(['Need to fill out']) : setPage(2)}>Next:Project Title</button>
+          {/* </label> */}
+          <div className='new-project-buttons'>
+            {formType === 'create' && <h1>Getting started with your project!</h1>}
+            <button className='new-project-form-button' disabled={!project?.category_id} onClick={()=> !project?.category_id ? setErrors(['Need to fill out']) : setPage(2) || setErrors([])}>Next: Project Title</button>
+          </div>
         </>
       )
     } else if(page === 2){
       return(
         <>
-          <label>Project Title
-            <input
-              type="text"
-              value={project?.title}
-              placeholder="Your Memoirs" 
-              onChange={update('title')}
-              className="project-field"
-            />
-          </label>
-          <label>Project Description
+          <h1 className="new-project-title">Give your project a title.</h1>
+          <h2 className="new-project-subtitle">Include a project description so people will know what your project is about.</h2>
+          {/* <label>Project Title */}
+            <div className="input-container">
+              <input
+                type="text"
+                value={project?.title}
+                // placeholder="Your project title" 
+                onChange={update('title')}
+                className="project-field"
+              />
+              <label id={project?.title && 'filled'}>Project Title</label>
+            </div>
+          {/* </label> */}
+          {/* <label>Project Description */}
             <textarea
               value={project?.description}
-              placeholder="What is your project about?" 
+              placeholder="Describe what you'll be creating." 
               onChange={update('description')}
               className="project-field"
             />
-          </label>
+          {/* </label> */}
           <div className='project-errors'>{renderErrors()}</div>
-          <button onClick={()=> setPage(1)}>Back:Project Category</button>
-          <button disabled={!project?.title || !project?.description} onClick={()=> setPage(3)}>Next:Project Campaign Details</button>
+          <div className='new-project-buttons'>
+            <button className='new-project-form-button' onClick={()=> setPage(1)}>Back:Project Category</button>
+            <button className='new-project-form-button' disabled={!project?.title || !project?.description} onClick={()=> setPage(3)}>Next:Project Campaign Details</button>
+          </div>
         </>
       )
     } else if(page === 3){
       return(
         <>
-          <label>Campaign (optional)
+          <h1 className="new-project-title">Build out your project with Campaign notes, Updates and FAQ's.</h1>
+          <h2 className="new-project-subtitle">These are all optional and you can add to them later.</h2>
+          {/* <label>Campaign (optional) */}
             <textarea 
               type="text"
               value={project?.campaign}
-              placeholder="What is the story and risks associated?" 
+              placeholder="Campaign: What is the story and risks associated?" 
               onChange={update('campaign')}
               className="project-field"
             />
-          </label>
+          {/* </label> */}
           
-          <label>Updates (optional)
+          {/* <label>Updates (optional) */}
             <textarea
               type="text"
               value={project?.updates}
-              placeholder="Anything you would like to share?" 
+              placeholder="Updates: Anything you would like to share?" 
               onChange={update('updates')}
               className="project-field"
             />
-          </label>
+          {/* </label> */}
           
-          <label>FAQ (optional)
+          {/* <label>FAQ (optional) */}
             <textarea
               type="text"
               value={project?.faq}
@@ -201,32 +217,40 @@ export default function ProjectForm(){
               onChange={update('faq')}
               className="project-field"
             />
-          </label>
+          {/* </label> */}
           <div className='project-errors'>{renderErrors()}</div>
-          <button onClick={()=> setPage(2)}>Back:Project Title</button>
-          <button onClick={()=> setPage(4)}>Next:Project Location</button>
+          <div className='new-project-buttons'>
+            <button className='new-project-form-button' onClick={()=> setPage(2)}>Back:Project Title</button>
+            <button className='new-project-form-button' onClick={()=> setPage(4)}>Next:Project Location</button>
+          </div>
         </>
       )
     }else if(page === 4){
       return(
         <>
-          <label>Location
+          <h1 className="new-project-title">Set a location for your project.</h1>
+          <h2 className="new-project-subtitle">Give your city and country of legal residence if you’re raising funds as an individual. If you’re raising funds for a business or nonprofit, select the country where the entity’s tax ID is registered.</h2>
+          {/* <label>Location */}
             <input
               type="text"
               value={project?.location}
-              placeholder="Where you at?" 
+              placeholder="Location" 
               onChange={update('location')}
               className="project-field"
             />
-          </label>
+          {/* </label> */}
           <div className='project-errors'>{renderErrors()}</div>
-          <button onClick={()=> setPage(3)}>Back: Campaign Details</button>
-          <button disabled={!project?.location} onClick={()=> setPage(5)}>Next: Project Dates and Goal</button>
+          <div className='new-project-buttons'>
+            <button className='new-project-form-button' onClick={()=> setPage(3)}>Back: Campaign Details</button>
+            <button className='new-project-form-button' disabled={!project?.location} onClick={()=> setPage(5)}>Next: Project Dates and Goal</button>
+          </div>
         </>
       )  
     }else if (page === 5){
       return(
         <>
+          <h1 className="new-project-title">Next, set preliminary start and end dates for your funding period as well as the funding goal.</h1>
+          <h2 className="new-project-subtitle">These can be updated before your project funding launch date.</h2>
           <label>Project Start Date
             <input
               type='date'              
@@ -247,22 +271,25 @@ export default function ProjectForm(){
             <input required
               type="currency"
               value={project?.funding_goal}
-              placeholder="Be realistc"
+              placeholder="Funding Goal"
               onChange={update('funding_goal')}
               className="project-field"
             />
           </label>
 
           <div className='project-errors'>{renderErrors()}</div>
-          <button onClick={()=> setPage(4)}>Back: Location</button>
-          <button disabled={!project?.start_date || !project?.end_date || !project?.funding_goal} onClick={()=> setPage(6)}>Next: Project Image</button>
-        
+          <div className='new-project-buttons'>
+            <button className='new-project-form-button' onClick={()=> setPage(4)}>Back: Location</button>
+            <button className='new-project-form-button' disabled={!project?.start_date || !project?.end_date || !project?.funding_goal} onClick={()=> setPage(6)}>Next: Project Image</button>
+          </div>
         </>
       )
     } else if(page === 6){
       return(
         <>
-          <label>Upload Project Image (optional)
+          <h1 className="new-project-title">Last one - upload a cover photo for your project.</h1>
+          <h2 className="new-project-subtitle">This is optional but you should add one before your project launches.</h2>
+          {/* <label>Upload Project Image (optional) */}
             <div className='picture-container'>
               <input
               className='picture-input'
@@ -272,12 +299,14 @@ export default function ProjectForm(){
               {preview}
             </div>
           
-          </label>
+          {/* </label> */}
           { project.imageUrl ? <button onClick={()=> setProject({...project, imageUrl: ''})}>Cancel</button>: null}
 
           <div className='project-errors'>{renderErrors()}</div>
-          <button onClick={()=> setPage(5)}>Back: Project Dates and Goal</button>
-          <button onClick={handleSubmit}>Finalize</button>
+          <div className='new-project-buttons'>
+            <button className='new-project-form-button' onClick={()=> setPage(5)}>Back: Project Dates and Goal</button>
+            <button className='new-project-form-button' onClick={handleSubmit}>Finalize</button>
+          </div>
         </>
       )
     }
@@ -286,11 +315,11 @@ export default function ProjectForm(){
 
     return(
       <div className="new-project-container">
-      <h1 className="new-project-title">Select a primary category for your project.</h1>
-      <h2>These will help backers find your project, and you can change them later if you need to.</h2>
-      <div className="new-project-form">
-        {formContents()}
-      </div>
+        <strong className='new-project-pagination'>{page} of 6</strong>
+        <div className="new-project-form">
+          {formContents()}
+        </div>
+        <div className="new-project-fineprint">Please note: Your ability to edit, hide, or delete a project is limited after you launch a project.</div>
     </div>
     )
         
